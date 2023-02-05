3aede040bc929e0d833512d5282d1c5f925dcde6
Tyler <hilltyler49@gmail.com>
2023-02-05 02:01:46 -0600

    updated gitignore

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // TODO: Create an array of questions for user input
-const questions = inquirer.prompt([
+const questions = [
   {
     type: "input",
     name: "projectTitle",
@@ -55,7 +55,7 @@ const questions = inquirer.prompt([
     name: "githubUsername",
     message: "Enter your GitHub username:",
   },
-]);
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
